8bc1e86db8242c76e66769f7310d7216f32e67d8
behayluN <addisubehaylu@gmail.com>
2024-10-22 02:36:22 +0300

    final commits

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -7,7 +7,7 @@ function Main() {
     return (
         <div>
             <Helmet>
-                <title>{headerData.name} - Porfolio</title>
+                <title>{headerData.name}</title>
             </Helmet>
             <Navbar />        
             <Landing />
